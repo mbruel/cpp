e367bdeeb30c70ebc6be92441b86fcbe50714e84
Matthieu <Matthieu.Bruel@gmail.com>
2015-11-17 18:26:37 +0000

    word to permute as argument and display the number of permutations

--- permute.cpp
+++ permute.cpp
@@ -1,21 +1,35 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
-void permute(char str[], int pos, int last){
-	if (pos == last)
+void permute(char str[], int pos, int last, int *nbPermute){
+	if (pos == last){
 		cout << str << endl;
+		++(*nbPermute);
+	}
 	else{
 		for (int i = pos; i <= last; ++i){
 			swap(str[pos], str[i]);
-			permute(str, pos+1, last);
+			permute(str, pos+1, last, nbPermute);
 			swap(str[pos], str[i]);
 		}
 	}
 }
 
-int main()
+int main(int argc, char* argv[])
 {
-	char str[] = "CAT";
-	permute(str, 0, 2);
+
+	if (argc != 2){
+		cout << "Syntax: " << argv[0]
+			 << " <string to give the permutations of>\n";
+		return 1;
+	}
+
+	char *str = argv[1];
+	int len = strlen(str);
+
+	int nbPermute = 0;
+	permute(str, 0, len-1, &nbPermute);
+	cout << "There are " << nbPermute << " different permutations\n";
 	return 0;
 }
